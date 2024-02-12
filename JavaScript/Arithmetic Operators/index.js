06460204587701cfba6f897787b0bf674418103a
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-12 11:36:21 -0300

    Arithimetics Operators
    Arithimetics Operators

---
+++ JavaScript/Arithmetic Operators/index.js
@@ -0,0 +1,29 @@
+// Arithmetic Operators = operands (values, variablels etc.)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5;
+
+let students = 30;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// let extraStudents = students % 3;
+
+//students++;
+//students--;
+
+
+/*
+  Operator Precendence
+  1. parenthesis ()
+  2. exponents
+  3. multiplication & division & modulo
+  4. addition & subtraction
+*/
+
+//let result = 1 + 2 * 3 + 4 ** 2;
+let result = 6 / 128;
+
+console.log(result);
