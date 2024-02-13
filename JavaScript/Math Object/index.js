231c3faf46bb3af5e594049bfab022714f44143a
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-13 05:41:59 -0300

    Math Object
    Math Object

---
+++ JavaScript/Math Object/index.js
@@ -0,0 +1,29 @@
+// Math = build-in object tha provides a 
+//        collection of properties and methods
+
+let x = 3
+let y = 2
+let z = 1
+
+
+// z = Math.round(x) 
+// z = Math.floor(x) round up
+// z = Math.ceil(x) round down
+//z = Math.trunc(x) remove decimals
+ 
+// z = Math.pow(x, y) **
+
+// z = Math.sqrt(x) raiz²
+
+// z = Math.log(x)
+// z = Math.sin(x)
+// z = Math.cos(x)
+// z = Math.tan(x)
+
+// z = Math.abs(x) +value
+// z = Math.sign(x) -1(negative) 1(positive) 0(0)
+
+// let max = Math.max(x, y, z) max value
+// let min = Math.min(x, y, z) min value
+
+console.log(min)
