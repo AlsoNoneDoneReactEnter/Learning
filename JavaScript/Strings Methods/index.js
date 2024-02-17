7143813d7abbe9ebfc83e98ada190945fa7c189f
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-17 10:21:49 -0300

    Strings Method
    Strings Method

---
+++ JavaScript/Strings Methods/index.js
@@ -0,0 +1,21 @@
+// string methods = allow you to manipulate and work with text (strings)
+
+let userName = "BroCode"
+
+console.log(userName.charAt(0)) // get the fix character by index
+console.log(userName.indexOf("r")) // get the index by character
+console.log(userName.lastIndexOf("o")) // get the last index by character
+console.log(userName.length) // get the length of string
+console.log(userName.trim) // delete spaces in string             //needs assing to variable?
+console.log(userName.toUpperCase) // Make the string UpperCase   //needs assing to variable?   
+console.log(userName.toLowerCase) // Make the string LowerCase      //needs assing to variable?
+console.log(userName.repeat(2)) // Repeat strings how many times you want
+console.log(userName.startsWith(" ")) // Repeat strings how many times you want
+console.log(userName.endsWith(" ")) // Repeat strings how many times you want
+console.log(userName.includes(" ")) // The string include ()?
+console.log(userName.replace("o", "a")) // replace
+console.log(userName.padStart(15, "0")) // start with "" string
+console.log(userName.padEnd(15, "0")) // end with "" string
+
+
+
