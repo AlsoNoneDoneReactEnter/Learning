e541d163b01bf36f2702b1af0e773ca12cda4446
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-18 05:15:21 -0300

    Logical Operators
    Logical Operators

---
+++ JavaScript/Logical Operators/index.js
@@ -0,0 +1,24 @@
+// logical operators = used to combine or manipulate boolean values
+//                     (true or false)
+
+//                      AND = &&
+//                      OR = ||
+//                      NOT !  //true to be false or false to be true
+
+// const temp = 20
+// 
+// if (temp > 0 || temp <= 30) {
+//     console.log("The weather is good")
+// }
+// else{
+//     console.log("The weather is BAD")
+// }
+
+const isSunny = false
+
+if (!isSunny){
+    console.log("It is Sunny")
+}
+else{
+    console.log("It is Cloud")
+}
