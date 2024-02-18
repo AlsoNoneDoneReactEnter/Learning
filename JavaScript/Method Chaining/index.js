7f32238634b741f4bd5514a6bcd119649b15a411
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-17 22:02:17 -0300

    Method Chaining
    Method Chaining

---
+++ JavaScript/Method Chaining/index.js
@@ -0,0 +1,22 @@
+// Method chaining = calling one method after another in one continuos line of code
+
+
+let username = window.prompt("Enter yout username: ")
+
+// ----------- NO METHOD CHAINING -------------
+
+// username = username.trim()
+// let letter = username.charAt(0)
+// letter = letter.toUpperCase()
+// 
+// let extraChars = username.slice(1)
+// extraChars = extraChars.toLowerCase()
+// 
+// username = letter + extraChars
+// 
+// console.log(username)
+
+// ----------- METHOD CHAINING -------------
+
+username = username .trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase()
+console.log(username)
