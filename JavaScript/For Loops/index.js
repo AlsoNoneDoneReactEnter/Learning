f1cf6097af99b458ed200ce6d7f2fc2ddb6dc50f
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-18 05:45:35 -0300

    For Loops

---
+++ JavaScript/For Loops/index.js
@@ -0,0 +1,24 @@
+
+// for (i = 10; i > 0; i--){
+//     console.log(i)
+// }
+// 
+// console.log("Happy new Year")
+
+// for (let i = 1; i <= 20; i++){
+//     if(i == 13){
+//         continue
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+for (let i = 1; i <= 20; i++){
+    if(i == 13){
+        break
+    }
+    else{
+        console.log(i)
+    }
+}
