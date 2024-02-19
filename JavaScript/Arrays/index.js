1bfacbf20ca5ff3713a1869f12397a72c27cf943
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-19 19:43:23 -0300

    Arrays
    Arrays

---
+++ JavaScript/Arrays/index.js
@@ -0,0 +1,35 @@
+
+
+let fruits = ["apple", "orange", "banana", "coconut"]
+
+// fruits.push("coconut")
+// fruits.pop()
+// fruits.unshift("mango")
+// fruits.shift()
+
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+console.log("-------------");
+
+for(let i = fruits.length - 1; i >= 0; i--){
+    console.log(fruits[i]);
+}
+
+console.log("-------------");
+
+fruits.sort().reverse()
+
+for(let fruit of fruits){
+    console.log(fruit)
+}
+
+console.log("-------------");
+
+
+
+// let numOfFruits  = fruits.length
+// let index = fruits.indexOf("mango") // if define to a undefiend index return -1
+// console.log(index);
