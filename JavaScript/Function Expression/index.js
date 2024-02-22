6ca1c003787816323505cc54cb0105a01df1cc25
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-22 20:36:21 -0300

    Function Expression
    Function Expression

--- JavaScript/Function Expression/index.js
+++ JavaScript/Function Expression/index.js
@@ -5,7 +5,10 @@
 // }
 
 // function expressions = a way to define functions as values or variables
-
+// 1. Callbacks in asynchronous operations
+// 2. Higher-Order Functions
+// 3. Closures
+// 4. Event Listeners
 
 // setTimeout(function() {
 //     console.log("Hello!");
@@ -25,8 +28,10 @@ const evenNums = numbers.filter(function(element) {
 const oddNums = numbers.filter(function(element) {
     return element % 2 !== 0 
 })
-
-console.log(oddNums);
+const total = numbers.reduce(function (accumulator, element) {
+    return accumulator + element
+})
+console.log(total);
 
 
 
