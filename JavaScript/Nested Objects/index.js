a523809e38eb45376782ded1ed6b1442ad8d1e06
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-26 17:43:12 -0300

    Nested Objects
    Nested Objects

--- JavaScript/Nested Objects/index.js
+++ JavaScript/Nested Objects/index.js
@@ -5,16 +5,50 @@
 
 
 
-const person = {
-    fullName: "Spongebob Squarepants",
-    age: 30,
-    isStudent: true,
-    hobbies: ["karate", "jellyfishing", "cooking"],
-    address: {
-        street: "124 Conch St.",
-        city: "Bikini Bottom",
-        country: "Int. Water"
+// const person = {
+//     fullName: "Spongebob Squarepants",
+//     age: 30,
+//     isStudent: true,
+//     hobbies: ["karate", "jellyfishing", "cooking"],
+//     address: {
+//         street: "124 Conch St.",
+//         city: "Bikini Bottom",
+//         country: "Int. Water"
+//     }
+// }
+
+// for(const property in person.address){
+//     console.log(person.address[property]);
+// }
+ 
+// console.log(person.fullName);
+// console.log(person.age);
+// console.log(person.isStudent);
+// console.log(person.hobbies[2]);
+// console.log(person.address.city);
+
+class Person{
+
+    constructor(name, age, ...address){
+        this.name = name;
+        this.age = age;
+        this.address = new Address(...address);
     }
 }
 
-console.log(person.fullName);
+
+class Address{
+
+    constructor(street, city, country){
+        this.street = street;
+        this.city = city;
+        this.country = country;
+
+    }
+}
+
+const person1 = new Person("Spongebob", 30, "124 Conch St.", "Bikini Bottom", "Int. Waters")
+const person2 = new Person("Patrick", 34, "128 Conch St.", "Bikini Bottom", "Int. Waters")
+const person3 = new Person("Squidward", 55, "126 Conch St.", "Bikini Bottom", "Int. Waters")
+
+console.log(person3.name);
