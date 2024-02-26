b646ebb2a65bbb53477edaf57d6813f479a1651d
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-25 21:45:36 -0300

    Nested Objects
    Nested Objects

---
+++ JavaScript/Nested Objects/index.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+const person = {
+    fullName: "Spongebob Squarepants",
+    age: 30,
+    isStudent: true,
+    hobbies: ["karate", "jellyfishing", "cooking"],
+    address: {
+        street: "124 Conch St.",
+        city: "Bikini Bottom",
+        country: "Int. Water"
+    }
+}
+
+console.log(person.fullName);
