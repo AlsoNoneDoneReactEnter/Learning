d0447d878a1bf99c0158dbaf8046753252d92f69
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-27 06:39:15 -0300

    Dates
    Dates

---
+++ JavaScript/Dates/index.js
@@ -0,0 +1,34 @@
+// Date (year, month, day, hour, minute, second, ms)
+// const date = new Date();
+
+// const year = date.getFullYear()
+// const month = date.getMonth()
+// const day = date.getDate()
+// const hour = date.getHours()
+// const minutes = date.getMinutes()
+// const seconds = date.getSeconds()
+// const dayOfWeek = date.getDay()
+
+// console.log(year);
+// console.log(month);
+// console.log(day);
+// console.log(hour);
+// console.log(minutes);
+// console.log(seconds);
+// console.log(dayOfWeek);
+
+
+// date.setFullYear(2025)
+// date.setMonth(0)
+// date.setDate(1)
+// date.setHours(2)
+// date.setMinutes(2)
+// date.setSeconds(3)
+// console.log(date);
+
+const date1 = new Date("2023-12-31")
+const date2 = new Date("2024-01-01")
+
+if(date2 > date1){
+    console.log('HAPPY NEW YEAR!');
+}
