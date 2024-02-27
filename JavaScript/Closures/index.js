604f3479d63ca79c2736a7c9b7068a3401a38e00
Flynn <121909668+AlsoNoneDoneReactEnter@users.noreply.github.com>
2024-02-27 06:52:37 -0300

    Closures
    Closures

---
+++ JavaScript/Closures/index.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+// function outer() {
+    
+//     let message = "Hello"
+
+//     function inner() {
+//         console.log(message);
+//     }
+
+//     inner()
+// }
+
+// outer();
+
+// function createCounter() {
+    
+//     let count = 0;
+
+
+//     function increment() {
+//         count++;
+//         console.log(`Count increased to ${count}`);
+//     }
+
+//     function getCount() {
+//         return count;
+//     }
+
+//     return {increment, getCount}
+// }
+
+// const counter = createCounter()
+// counter.increment()
+// counter.increment()
+
+// console.log(`The current Count is ${counter.getCount()}`);
+
+function createGame() {
+    let score = 0;
+
+    function increaseScore(points) {
+        score += points;
+        console.log(`+${points}pts`);
+    }
+
+    function decreaseScore(points) {
+        score -= points;
+        console.log(`-${points}pts`);
+    }
+
+    function getScore(params) {
+        return score
+    }
+
+    return {increaseScore, decreaseScore, getScore}
+}
+
+const game = createGame()
+
+
+game.increaseScore(5)
+game.increaseScore(6)
+game.decreaseScore(3)
+console.log(`The final score is ${getScore()}`);
